92019481fb9826c93f87da21af15b3a457c60907
Segun Odusina <odusinaoluwasegun@gmail.com>
2023-03-19 18:13:17 +0100

    Delete film_rev.sql

--- film_rev.sql
+++
@@ -1,7 +0,0 @@
-/* Query for filmm revenue */
-SELECT f.title AS FILM_TITLE, SUM(p.amount) AS  FILM_REVENUE FROM payment p
-JOIN rental r ON p.rental_id = r.rental_id
-JOIN inventory i ON r.inventory_id = i.inventory_id
-JOIN film f ON i.film_id = f.film_id
-GROUP BY f.title
-ORDER BY SUM(p.amount) DESC
